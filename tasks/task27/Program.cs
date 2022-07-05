e407b49bdfce2c0c76c09185de4fa241ce6ce30b
Anna Kerps <anya-kerps@bk.ru>
2022-07-05 14:41:09 +0300

    Задача 27

---
+++ tasks/task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int SumCalculation(int inputNumber)
+{
+    int result = 0;
+    while (inputNumber > 0)
+    {
+        result = result + inputNumber % 10;
+        inputNumber = inputNumber / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int output = SumCalculation(number);
+Console.WriteLine($"{number} -> {output}");
+
+
+
